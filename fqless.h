8fd7a36c483e61b05904f9e51f061aab37e6ceff
openpaul <paulsocial@gmx.de>
2017-02-13 17:27:03 +0100

    add new fqless files

---
+++ fqless.h
@@ -0,0 +1,25 @@
+
+#ifndef FQLESS_H
+#define FQLESS_H
+
+class fqless{
+     public:
+        int ch;
+        string colorMessage = "";
+        WINDOW * Wtext;
+        WINDOW * Wcmd;
+        // init the class
+        fqless(options*); 
+
+        static void quit();
+        void initTheColors(std::pair<uint, uint> p);
+        void winInit(options * opts);
+        color IntToColor(int i, std::pair<uint, uint> p);
+            
+        
+    
+    private:
+        void fillPad(options* opts, fastq* FQ, int dir);
+};
+
+#endif
